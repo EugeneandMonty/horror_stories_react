186fc878374a7f03aac2a5a78ac335622af4b5f6
EugeneandMonty <112835313+EugeneandMonty@users.noreply.github.com>
2022-10-18 01:05:37 -0500

    improved

--- src/Section.js
+++ src/Section.js
@@ -2,21 +2,7 @@ import React from 'react'
 import StoryCard from './StoryCard'
 import data from './data'
 
-  
-
 const Section = () => {
-
-let len = data.length;
-let numbers = []; 
-let data_changed = [];
-
-for (let i=0; i<len; i++) {
-  if (i % 8 === 0 && i !== 0) {
-    numbers.push(i);
-    data_changed = data.slice(i, len);
-    // console.log(data_changed);
-  }
-}
 
   return (     
 
